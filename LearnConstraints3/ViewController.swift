e70728f299286adfefaec3ad611f6c9592228f3e
Almaz Sultanbai <aldeco312@gmail.com>
2024-03-06 14:41:47 +0600

    Initial Commit

---
+++ LearnConstraints3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnConstraints3
+//
+//  Created by Sultanbai Almaz on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
